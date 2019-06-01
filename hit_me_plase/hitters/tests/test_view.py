e1fab41510fc2299c69b9b8cd1704e2f378ca9c8
saowaluck <pop@prontomaarketing.com>
2019-06-01 15:19:32 +0700

    :white_check_mark: Fix test h1 show hit me :panda_face:

--- hit_me_plase/hitters/tests/test_view.py
+++ hit_me_plase/hitters/tests/test_view.py
@@ -7,6 +7,9 @@ class LandingPageViewTest(TestCase):
     def test_should_have_form_with_have_email_field_and_submit_button(self):
         response = self.client.get('/')
 
+        expected = '<h1>Hit Me please!</h1>'
+        self.assertContains(response, expected, status_code=200)
+
         expected = '<form action="." method="post">'
         self.assertContains(response, expected, status_code=200)
 
--- tests/cypress/integration/submit_form.spec.js
+++ tests/cypress/integration/submit_form.spec.js
@@ -1,5 +1,14 @@
 describe('Landing Page', () => {
     it('Should be able to submit form with email', () => {
+        const user = cy
+        user.visit('http://localhost:8000')
+        user.get('h1')
+            .contains('Hit Me please!')
+        user.get('input[name="email"]')
+            .type('pop@prontomarketing.com')
+        user.get('input[type="submit"]')
+            .click()
+
         const admin = cy
         admin.visit('http://localhost:8000/admin/')
         admin.get('input[name="username"]')
