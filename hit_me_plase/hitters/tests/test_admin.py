18045008e81b01b9514207d75853ab052a1cf9dc
saowaluck <pop@prontomaarketing.com>
2019-06-01 13:32:34 +0700

    :art: Separate test to tests folder :panda_face:

--- hit_me_plase/hitters/tests/test_admin.py
+++ hit_me_plase/hitters/tests/test_admin.py
@@ -1,14 +1,8 @@
 from django.contrib import admin
 from django.test import TestCase
 
-from .models import Hitter
-from .admin import HitterAdmin
-
-
-class HitterTest(TestCase):
-    def test_hitter_model_should_have_email_field(self):
-        hitter = Hitter.objects.create(email='pop@prontomarketing.com')
-        self.assertEqual(hitter.email, 'pop@prontomarketing.com')
+from ..admin import HitterAdmin
+from ..models import Hitter
 
 
 class HitterAdminTest(TestCase):
---
+++ hit_me_plase/hitters/tests/tests_model.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+
+from ..models import Hitter
+
+
+class HitterTest(TestCase):
+    def test_hitter_model_should_have_email_field(self):
+        hitter = Hitter.objects.create(email='pop@prontomarketing.com')
+        self.assertEqual(hitter.email, 'pop@prontomarketing.com')
