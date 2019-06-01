309a98c14f5b372a8cbcca79796e7c711dac8988
saowaluck <pop@prontomaarketing.com>
2019-06-01 11:37:15 +0700

    :white_check_mark: Add test admin :panda_face:

--- hit_me_plase/hitters/tests.py
+++ hit_me_plase/hitters/tests.py
@@ -1,8 +1,16 @@
+from django.contrib import admin
 from django.test import TestCase
+
 from .models import Hitter
+from .admin import HitterAdmin
 
 
 class HitterTest(TestCase):
     def test_hitter_model_should_have_email_field(self):
         hitter = Hitter.objects.create(email='pop@prontomarketing.com')
         self.assertEqual(hitter.email, 'pop@prontomarketing.com')
+
+
+class HitterAdminTest(TestCase):
+    def test_hitter_should_be_register_to_admin(self):
+        self.assertIsInstance(admin.site._registry[Hitter], HitterAdmin)
