7deb2865f4529b6e35ef9cc486819c306c3763cf
saowaluck <pop@prontomaarketing.com>
2019-06-02 10:37:33 +0700

    Update teraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,5 +2,30 @@ provider "google" {
   credentials = "${file("hitmebaby.json")}"
   project     = "trusty-solution-240102"
   region      = "asia-southeast1"
-  zone        = "asia-southeast-b"
+  zone        = "asia-southeast1-b"
+}
+resource "google_compute_instance" "tf_instance" {
+  name         = "tf-instance"
+  machine_type = "n1-standard-1"
+
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1804-bionic-v20190530"
+      size  = 30
+    }
+  }
+  metadata = {
+    sshKeys = "pop:${file("~/.ssh/hitme.pub")}"
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+
+  tags = ["my-web"]
+}
+output "ip" {
+  value = "${google_compute_instance.tf_instance.*.network_interface.0.access_config.0.nat_ip}"
 }
